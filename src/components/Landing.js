c66d7ce2e915be25c759be1135b2288cde4e4fdc
Rom Vic <technoyguy@gmail.com>
2018-11-30 12:56:26 -0800

    adjust css for logo

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -216,40 +216,19 @@ const Landing = () =>
         <div className="row">
           <div className="col-lg-12 text-center">
             <h2 className="section-heading text-uppercase">Contact Us</h2>
-            <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+            <h3 className="section-subheading text-muted hidden">At Vignam Systems, we build long term relationships with our technical professionals .</h3>
           </div>
         </div>
         <div className="row">
-          <div className="col-lg-12">
-            <form id="contactForm" name="sentMessage" noValidate="noValidate">
-              <div className="row">
-                <div className="col-md-6">
-                  <div className="form-group">
-                    <input className="form-control" id="name" type="text" placeholder="Your Name *" required="required" data-validation-required-message="Please enter your name." ></input>
-                    <p className="help-block text-danger"></p>
-                  </div>
-                  <div className="form-group">
-                    <input className="form-control" id="email" type="email" placeholder="Your Email *" required="required" data-validation-required-message="Please enter your email address."/>
-                    <p className="help-block text-danger"></p>
-                  </div>
-                  <div className="form-group">
-                    <input className="form-control" id="phone" type="tel" placeholder="Your Phone *" required="required" data-validation-required-message="Please enter your phone number."/>
-                    <p className="help-block text-danger"></p>
-                  </div>
-                </div>
-                <div className="col-md-6">
-                  <div className="form-group">
-                    <textarea className="form-control" id="message" placeholder="Your Message *" required="required" data-validation-required-message="Please enter a message."></textarea>
-                    <p className="help-block text-danger"></p>
-                  </div>
-                </div>
-                <div className="clearfix"></div>
-                <div className="col-lg-12 text-center">
-                  <div id="success"></div>
-                  <button id="sendMessageButton" className="btn btn-primary btn-xl text-uppercase" type="submit">Send Message</button>
-                </div>
-              </div>
-            </form>
+          <div className="col-lg-12 text-center">
+            <h4 className="service-heading" style={{color:"white"}}>We build long term relationships with our technical professionals by</h4>
+            <ul className="text-muted" style={{fontStyle:"italic",textAlign:"left"}}>
+              <li> Consistently providing them with great career opportunities. </li>
+              <li> Providing opportunities to excel and grow.</li>
+              <li>Matching their technical and industry experience to jobs worthy of their abilities </li>
+              <li> Vignam Systems values its employees and offers a competitive package.</li>
+            </ul>
+            <p  style={{color:"white"}}>Submit a detailed resume with education, Project experience and geographic preference at the following address via email: <a href="mailto:resume@vignamsys.com?subject=Resume of ">resume@vignamsys.com</a></p>
           </div>
         </div>
       </div>
